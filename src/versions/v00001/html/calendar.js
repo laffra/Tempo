e3ee8d4ad60cbd930f90c72a52a95c31509534aa
Chris Laffra <laffra@uber.com>
2019-07-24 07:46:05 +0200

    Improve hour color and minutes

--- src/versions/v00001/html/calendar.js
+++ src/versions/v00001/html/calendar.js
@@ -13,6 +13,7 @@ var INDEX_URL = 9;
 
 var MONTH_NAMES = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var DAY_NAMES = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var lastEvents = [];
 
 function render() {
   console.log(new Date(), "render");
@@ -26,7 +27,7 @@ function parseEvents(events) {
   var max = 0;
   for (var n = 0; n < events.length; n++) {
     var event = events[n];
-    var timestamp = event[0];
+    var timestamp = event[INDEX_TIMESTAMP];
     var date = new Date(timestamp * 1000);
     var weekIndex = Math.floor(date.getDate() / 7);
     var dayIndex = date.getDay();
@@ -49,13 +50,16 @@ function renderEvents() {
   var start = currentDate.getTime()/1000;
   var end = start + 60 * 60 * 24 * 31;
   $.get("http://localhost:1187/events?start=" + start + "&end=" + end, function(events) {
+    lastEvents = events;
     var eventMap = parseEvents(events);
     var max = eventMap["max"];
     for (var key in eventMap) {
       if (key == "max") continue;
       var hours = $("<div>");
       for (var hour = 0; hour < 24; hour++) {
-        var color = 256 - Math.min(256, Math.floor(256 * eventMap[key][hour].length / max));
+        var coverage = getSampleCoverage(eventMap[key][hour]);
+        var color = 256 - Math.floor(coverage * 256);
+        var minutes = Math.floor(coverage * 60);
         hours.append($("<div>")
           .addClass("hour")
           .css("background-color", "rgb(" + color + "," + color + "," + color + ")")
@@ -63,10 +67,12 @@ function renderEvents() {
           .attr("key", key)
           .attr("day", $("#cell" + key).attr("day"))
           .attr("hour", hour)
+          .attr("minutes", minutes)
           .click(function () {
             var key = $(this).attr("key");
             var hour = $(this).attr("hour");
-            showDetails(parseInt(key[0]), parseInt(key[1]), parseInt(hour), eventMap[key], $(this));
+            var minutes = $(this).attr("minutes");
+            showDetails(eventMap[key], key, parseInt(hour), minutes, $(this));
           })
         );
       }
@@ -77,10 +83,26 @@ function renderEvents() {
   })
 }
 
-function showDetails(weekIndex, dayIndex, hour, day, dayDiv) {
+function getSampleCoverage(events) {
+  var samples = {}
+  for (var n = 0; n < events.length; n++) {
+    var event = events[n];
+    var timestamp = event[INDEX_TIMESTAMP];
+    for (var k = -3; k < 3; k++) {
+      samples[timestamp + k] = event;
+    }
+  }
+  return Object.keys(samples).length / 3600;
+}
+
+function showMatching(label) {
+  // TODO
+}
+
+function showDetails(dayEvents, key, hour, minutes, dayDiv) {
   var eventCountByUser = {};
-  for (var n = 0; n < day[hour].length; n++) {
-    var event = day[hour][n];
+  for (var n = 0; n < dayEvents[hour].length; n++) {
+    var event = dayEvents[hour][n];
     var key = event[INDEX_USER];
     if (key) {
       if (key.indexOf("@") == -1) {
@@ -92,19 +114,42 @@ function showDetails(weekIndex, dayIndex, hour, day, dayDiv) {
       eventCountByUser[key] = (eventCountByUser[key] || 0) + 1;
     }
   }
-  createPieChart(eventCountByUser, dayIndex, dayDiv.attr("day"), hour);
+  createPieChart(eventCountByUser, dayDiv.attr("day"), hour, minutes);
+  createDayChart(dayEvents);
+  showDetailsDialog();
 }
 
-function createPieChart(eventCountByUser, dayIndex, day, hour) {
+function showDetailsDialog() {
+  $("#details").dialog({
+    width: 1200,
+    height: 900,
+    modal: true,
+  });
+}
+
+function createDayChart(dayEvents) {
+  // TODO
+  for (var hour = 0; hour < 25; hour++) {
+    $("#details-day").append($("<div>")
+      .addClass("detail-hour-marker")
+      .html(hour < 24 ? hour : "&nbsp;"));
+  }
+}
+
+function createPieChart(eventCountByUser, day, hour, minutes) {
   var dataPoints = [];
   for (key in eventCountByUser) {
-    dataPoints.push({ label: key, value: eventCountByUser[key] });
+    var value = eventCountByUser[key];
+    if (value > 2) {
+      dataPoints.push({ label: key, value: value });
+    }
   }
-  $("#details").empty();
-  new d3pie("details", {
+  $("#details-pie").empty();
+  $("#details-day").empty();
+  new d3pie("details-pie", {
     header: {
       title: {
-        text: "Details for " + MONTH_NAMES[currentDate.getMonth()] + " " + day + " - " + hour + ":00 - " + (hour+1) + ":00",
+        text: "Details for " + minutes + " minutes during " + MONTH_NAMES[currentDate.getMonth()] + " " + day + " - " + hour + ":00 - " + (hour+1) + ":00",
         fontSize: 30,
         margin: 24,
       },
@@ -132,13 +177,14 @@ function createPieChart(eventCountByUser, dayIndex, day, hour) {
       },
     },
     data: {
-      content: dataPoints
+      content: dataPoints,
+      sortOrder: "value-asc",
+    },
+    callbacks: {
+      onClickSegment: function(segment) {
+        showMatching(segment.data.label);
+      }
     }
-  });
-  $("#details").dialog({
-    width: 1200,
-    height: 900,
-    modal: true,
   });
 }
 
