18db2c64db48a1f55f8dc9568610cbe1167f47c1
Chris Laffra <laffra@uber.com>
2019-07-24 09:09:40 +0200

    Publish version 0.1.0

---
+++ src/info.py
@@ -0,0 +1,3 @@
+version = '0.1.0'
+when = '2019-07-24'
+who = 'chris'
--- src/upload.py
+++ src/upload.py
@@ -1,8 +1,35 @@
-import shutil
-
 import os
+import shutil
+import sys
+from datetime import date
+
+def createLatestZip():
+    srcDir = os.path.dirname(__file__)
+    infoPath = os.path.join(srcDir, "info.py")
+
+    with open(infoPath) as fin:
+        print("Current version:\n")
+        print("=" * 90)
+        print(fin.read())
+        print("=" * 90)
+        print()
+
+    version = raw_input("What version do you want to publish? ")
+    yesno = raw_input("Generate zip for version %s (Y/N)? " % version)
+    if (yesno in ["Y", "y"]):
+        with open(infoPath, "w") as fout:
+            fout.write("version = '%s'\n" % version)
+            fout.write("when = '%s'\n" % date.today())
+            fout.write("who = '%s'\n" % os.path.expanduser("~").replace("/Users/", ""))
+
+        rootDir = os.path.dirname(srcDir)
+        zipPath = os.path.join(rootDir, "latest_dist")
+        shutil.make_archive(zipPath, 'zip', "src/latest")
+
+        print("Created latest zip in %s.zip:" % zipPath)
+        os.system("unzip -v %s.zip" % zipPath)
+        print("Next step: commit and push")
 
 
 if __name__ == "__main__":
-    path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "latest_dist")
-    shutil.make_archive(path, 'zip', "src/latest")
+    createLatestZip()
